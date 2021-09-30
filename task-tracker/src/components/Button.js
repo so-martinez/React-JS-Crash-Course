4e472e445d88e5151d055c8606e533a39f2ff393
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 13:50:10 -0300

    onClick event added

--- task-tracker/src/components/Button.js
+++ task-tracker/src/components/Button.js
@@ -2,9 +2,17 @@
 import PropTypes from 'prop-types'
 
 const Button = ({ color, text}) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
-        <button onClick={onClick} style={{backgroundColor: color}}
-        className='btn'>{text}</button>
+        <button
+            onClick={onClick}
+            style={{backgroundColor: color}}
+            className='btn'
+            >{text}
+        </button>
     )
 }
 
