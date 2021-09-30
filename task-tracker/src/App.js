5e352057cee1bfe9bf9fc40c2d70405ef49623af
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 18:35:54 -0300

    toggleTasks updated

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -58,9 +58,22 @@ const App = () => {
   }
 
   //Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
     setTasks(tasks.map((task) => task.id === id ?
-      {...task, reminder: !task.reminder} : task
+      {...task, reminder: data.reminder} : task
     ))
   }
 
