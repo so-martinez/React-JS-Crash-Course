f4948fd279097ba89dc624f185df8c3f8c03c200
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 18:52:06 -0300

    useLocation
    To hide the button depending of the location of the page. So if we are in /about the button is going to hide

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,17 +1,21 @@
 //first letter on uppercase is a convention for components
 //this used to be required but not anymore: import React from 'react'
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({title, onAdd, showAdd}) => {
+        const location = useLocation()
+
         return (
         <header className='header'>
             <h1>{title}</h1>
+            {location.pathname === '/' && (
             <Button
                 color={showAdd ? 'red' : 'green'}
                 text={showAdd ? 'Close' : 'Add'}
                 onClick={onAdd}
-            />
+            />)}
         </header>
     )
 }
