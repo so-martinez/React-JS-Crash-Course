6944fae1b11c17e3ec5bc428cb309a3579141c08
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 13:34:02 -0300

    styling fixed

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,11 +1,12 @@
-//uppercase H is a convention for components
+//first letter on uppercase is a convention for components
 //this used to be required but not anymore: import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
