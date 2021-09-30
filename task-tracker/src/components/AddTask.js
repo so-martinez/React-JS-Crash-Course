233d7b462c3af9f9993af7984b5de2059149fec0
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 15:22:09 -0300

    form input state added
    Now is fully functional

--- task-tracker/src/components/AddTask.js
+++ task-tracker/src/components/AddTask.js
@@ -1,17 +1,37 @@
+import { useState} from 'react'
+
 const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task'></input>
+                <input
+                    type='text'
+                    placeholder='Add Task'
+                    value={text}
+                    onChange={(elem) => setText(elem.target.value)}>
+                </input>
             </div>
             <div className='form-control'>
                 <label>Day & Time</label>
-                <input type='text' placeholder='Add Day & Time'></input>
+                <input
+                    type='text'
+                    placeholder='Add Day & Time'
+                    value={day}
+                    onChange={(elem) => setDay(elem.target.value)}>
+                </input>
             </div>
             <div className='form-control form-control-check'>
                 <label>Reminder</label>
-                <input type='checkbox'></input>
+                <input
+                    type='checkbox'
+                    value={reminder}
+                    onChange={(elem) => setReminder(elem.currentTarget.checked)}>
+                </input>
             </div>
 
             <input className='btn btn-block' type='submit' value='Save Task'></input>
