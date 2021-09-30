98713002c79a42e084172af8b1f1935d518fe24b
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 17:01:54 -0300

    Color toggle depending if the form is open or closed

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({title, onAdd, showAdd}) => {
         <header className='header'>
             <h1>{title}</h1>
             <Button
-                color='green'
+                color={showAdd ? 'red' : 'green'}
                 text={showAdd ? 'Close' : 'Add'}
                 onClick={onAdd}
             />
