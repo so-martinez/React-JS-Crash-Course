4302f39849d4d33d178532092308350ef7309ee7
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 13:13:01 -0300

    first function component added

---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+//uppercase H is a convention for components
+//this used to be required but not anymore: import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
