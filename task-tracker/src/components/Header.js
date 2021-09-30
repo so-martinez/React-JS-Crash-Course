3d3c05b32d0097b59f69d2610aa6f38b492ba08b
Sofía Martínez <somartinez@altimetrik.com>
2021-09-30 13:18:39 -0300

    another way to write props
    This way we don't need to add the "props.something"

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,10 +1,10 @@
 //uppercase H is a convention for components
 //this used to be required but not anymore: import React from 'react'
 
-const Header = (props) => {
+const Header = ({title}) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
